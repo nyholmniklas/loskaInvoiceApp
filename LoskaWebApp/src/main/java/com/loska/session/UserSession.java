6be50f775f2a04f785c6403f8da31601ac8adeef
Niklas <nyholmniklas@gmail.com>
2013-03-31 00:17:04 +0200

    Adding new invoice now adds user_id refkey. Created session scope UserSession bean to hold id.

--- LoskaWebApp/src/main/java/com/loska/session/UserSession.java
+++ LoskaWebApp/src/main/java/com/loska/session/UserSession.java
@@ -1,7 +1,31 @@
 package com.loska.session;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+import com.loska.model.user.User;
+
+@Component
+@Scope(value="session")
 public class UserSession {
+	private int userId;
+	
+	public UserSession(){
+		userId = 6;
+	}
 
+	public void setUserId(User user) {
+		setUserId(user.getUser_Id());
+		
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
 }
